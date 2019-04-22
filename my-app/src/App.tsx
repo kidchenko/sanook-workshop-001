71675abbc3dd9cc455637e20210247bfd963c91d
kidchenko <kidchenko@gmail.com>
2019-04-23 01:35:20 +0700

    Add css and state

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,25 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    name: '',
+    email: '',
+    message: ''
+  }
+
+  handleChange = (e: React.FormEvent<HTMLInputElement> | React.FormEvent<HTMLTextAreaElement>) => {
+    const { name, value } = e.target as any;
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+  submitForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    alert('Message sent.');
+    this.setState({
+      name: '',
+      email: '',
+      message: ''
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <section>
+          <h3 className="title">Hey! I'm Jose.</h3>
+          <form onSubmit={this.submitForm}>
+            <input type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+              id="name"
+              placeholder="Name"
+              className="form-field"
+              required
+              autoFocus />
+            <input type="email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+              id="email"
+              placeholder="Email"
+              className="form-field"
+              required />
+            <textarea
+              name="message"
+              value={this.state.message}
+              onChange={this.handleChange}
+              id="message"
+              placeholder="Message"
+              rows={4}
+              className="form-field"
+              required>
+            </textarea>
+            <input type="submit" value="Send Message" className="btn"></input>
+          </form>
+        </section>
       </div>
     );
   }
